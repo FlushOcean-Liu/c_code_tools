3e9e695a083faf117d9a2186785e4bb777aaa3dc
root <root@localhost.localdomain>
2022-05-08 09:46:53 -0400

    liugh:重新分类

---
+++ file_operation/read_write_file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int read_file(char *file_path)
+{
+
+
+
+    return 0;
+}
+
+int write_file(const char *src_file, const char *dst_file)
+
+
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+}
