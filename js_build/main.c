ab93135ebabdd3a69b8ffd25ca83c522623f3fc1
FlushOcean.Liu <1509444199@qq.com>
2022-05-13 22:44:38 +0800

    liugh:验证json包裹子json方式

--- js_build/main.c
+++ js_build/main.c
@@ -16,12 +16,17 @@ int main()
    jb_set_string(js, "timestamp", "202205122202");
 
    jb_set_uint(js, "sensor_id", 123456);
-   
+
+   jb_open_object(js,"http");
+   jb_set_string(js, "method","GET");
+   jb_set_string(js, "uri", "/");
+   jb_close(js);
+
    jb_close(js);
    printf("jslen:%d\n",jb_len(js));
    printf("%s\n",jb_ptr(js));
 
-
+   jb_free(js);
 
    return 0;
 }
