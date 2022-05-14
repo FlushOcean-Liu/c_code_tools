a1a9fba997f49d9ee63efd5df20b689de4cf49e1
FlushOcean.Liu <1509444199@qq.com>
2022-05-14 17:52:42 +0800

    liugh:socket增加select模式

--- socket/tcp_recv.c
+++ socket/tcp_recv.c
@@ -8,10 +8,13 @@
 
 #define DATA_BUFFER_SIZE  1024
 
+#define MAX_CLIENT        10000
+
 struct socket_args{
     int conn_fd;
 };
 
+#define MAX(x,y) ((x)>(y))?(x):(y)
 
 
 /*收包线程处理函数*/
@@ -39,30 +42,36 @@ void *socket_receive_thread(void *args)
 }
 
 
-int create_connect(void)
+int create_socket_thread(void)
 {
     pthread_t tid;
     struct sockaddr_in  client_addr,server_addr;
     
-    int  listen_fd=socket(AF_INET, SOCK_STREAM, 0);
     bzero(&server_addr,sizeof(server_addr));
     server_addr.sin_family= AF_INET;
-
     server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
     server_addr.sin_port=htons(9999);
 
-    bind(listen_fd, (struct sockaddr*)&server_addr,sizeof(server_addr));
-    listen(listen_fd, 10000);
+    int  sockfd=socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == 0) {
+        perror("init server socket error\n");
+        return -1;
+    }
+    
+    bind(sockfd, (struct sockaddr*)&server_addr,sizeof(server_addr));
+    listen(sockfd, MAX_CLIENT);
     socklen_t cli_len;
-    int conn_fd=0;
-    int ret=0;
+    int       conn_fd = 0;
+    int       ret     = 0;
+    
     while(1){
-        conn_fd=accept(listen_fd, (struct sockaddr*)&client_addr, &cli_len);
+        conn_fd=accept(sockfd, (struct sockaddr*)&client_addr, &cli_len);
         if(conn_fd<0){
-            printf("accept conn_fd < 0 ,=%d\n",conn_fd);
+            printf("accept conn_fd < 0 ,conn_fid=%d\n",conn_fd);
             usleep(1);
             continue;
         }
+        
         struct socket_args *child_args=(struct socket_args *)malloc(sizeof(struct socket_args *));
         child_args->conn_fd=conn_fd;
         if(child_args==NULL){
@@ -70,9 +79,6 @@ int create_connect(void)
             continue;
         }
         child_args->conn_fd=conn_fd;
-        //printf("[Client] get connection ip:%s,port:%d\n",
-        //                      inet_ntoa(client_addr.sin_addr),
-        //                      ntohs(client_addr.sin_port));
         ret=pthread_create(&tid, NULL,socket_receive_thread, (void*)child_args);
         if(0!=ret){
             printf("create thread failed!\n");
@@ -82,10 +88,97 @@ int create_connect(void)
 
 }
 
+int create_socket_select(void)
+{
+    struct sockaddr_in  client_addr,server_addr;
+    
+    bzero(&server_addr,sizeof(server_addr));
+    server_addr.sin_family= AF_INET;
+    server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+    server_addr.sin_port=htons(9999);
+
+    int  sockfd=socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == 0) {
+        perror("init server socket error\n");
+        return -1;
+    }
+
+    bind(sockfd, (struct sockaddr*)&server_addr,sizeof(server_addr));
+
+    listen(sockfd, MAX_CLIENT);
+    socklen_t cli_len;
+    int       conn_fd = 0;
+    int       ret     = 0;
+    int       i;
+    
+    int user_link[MAX_CLIENT];
+    int userfd[MAX_CLIENT];
+    char buffer[DATA_BUFFER_SIZE];
+
+    fd_set sockset;
+    int maxfd = 0;
+    struct timeval tv;
+    
+    listen(sockfd, MAX_CLIENT);
+    for (i = 0; i < MAX_CLIENT; i++) {
+        user_link[i] = 0;
+    }
+
+    FD_ZERO(&sockset);
+    FD_SET(sockfd, &sockset);
+
+    maxfd = MAX(maxfd, sockfd + 1);
+    int userindex = 0;
+    while(1) {
+        
+        select(maxfd, &sockset, NULL, NULL, &tv);
+        if (FD_ISSET(sockfd, &sockset)) {
+            conn_fd = accept(sockfd, (struct sockaddr *) &client_addr, &cli_len);
+            if (conn_fd <= 0)
+                perror("accept");
+            else {
+                user_link[userindex] = 1;
+                userfd[userindex] = conn_fd;
+                FD_SET(conn_fd, &sockset);
+                maxfd = MAX(maxfd, conn_fd + 1);
+            }
+        }
+
+        int ifd = 0;
+        for (i = 0; i < MAX_CLIENT; i++) {
+            if (user_link[i] == 1 && (FD_ISSET(userfd[i], &sockset))) {
+                memset(buffer, 0 ,DATA_BUFFER_SIZE);
+                unsigned int length = recv(userfd[i], buffer, DATA_BUFFER_SIZE, 0);
+                ifd++;
+                if (length <= 0) {
+                    close(userfd[i]);
+                    user_link[i] = 0;
+                    FD_CLR(userfd[i], &sockset);
+                } else {
+                    printf("RECV:%s\n",buffer);
+                }
+            }
+        }
+
+        FD_ZERO(&sockset);
+        FD_SET(sockfd, &sockset);
+        int j;
+        for (j = 0; j < MAX_CLIENT; j++) {
+            if (user_link[j] == 1)
+                FD_SET(userfd[j], &sockset);
+        }
+    }
+
+    return 0;
+ }
+
+
 int main()
 {
 
-    create_connect();
+    //create_socket_thread();
+
+    create_socket_select();
 
     return 0;
 }
